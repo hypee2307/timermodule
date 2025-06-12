2c7a8d4948bff4b4c9ca980a3e63c715d6daec63
hypee2307 <140878386+hypee2307@users.noreply.github.com>
2025-06-12 21:37:51 +0700

    Create rw1c_tisr.v

---
+++ testcases/rw1c_tisr.v
@@ -0,0 +1,21 @@
+task run_test();
+    begin
+        //Write TCMP value
+        apb_wr(ADDR_TCMP0, 32'h0000_00C8); //200 in decimal
+        apb_wr(ADDR_TCMP1, 32'h0000_0000);
+        apb_wr(ADDR_TIER, 32'h0000_0001); //enable timer interrupt
+        apb_wr(ADDR_TCR, 32'h0000_0001); //enable counter and disable div_en
+
+        repeat (200 - 3) @(posedge sys_clk); //delay until counter reach 5 in decimal
+        apb_rd(ADDR_TDR0, tb_rdata);
+        cmp_tim_int(1);
+
+        apb_wr(ADDR_TISR, 32'h0000_0000); //write 0 to bit 0 of TISR
+        cmp_tim_int(1);
+
+        apb_wr(ADDR_TISR, 32'h0000_0001); //write 1 to bit 0 of TISR
+        cmp_tim_int(0);
+
+        err_tb_check();
+    end
+endtask
